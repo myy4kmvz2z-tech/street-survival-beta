6a0c51f20c6e42d8a89c0b2485628ac02f69be84
myy4kmvz2z-tech <myy4kmvz2z@privaterelay.appleid.com>
2026-06-10 22:07:37 +0900

    Firebase Realtime Connect
    Updated Firebase configuration to enable Firebase and set new project values.

--- firebase-config.js
+++ firebase-config.js
@@ -2,16 +2,17 @@
 // ここをFirebaseの「ウェブアプリ設定」からコピーした値に貼り替えてください。
 // Firebase Console → Project settings → Your apps → Web app → firebaseConfig
 
-window.STREET_SURVIVAL_FIREBASE_ENABLED = false;
+window.STREET_SURVIVAL_FIREBASE_ENABLED = true;
 
 window.STREET_SURVIVAL_FIREBASE_CONFIG = {
-  apiKey: "PASTE_YOUR_API_KEY",
-  authDomain: "PASTE_YOUR_PROJECT.firebaseapp.com",
-  databaseURL: "https://PASTE_YOUR_PROJECT-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "PASTE_YOUR_PROJECT",
-  storageBucket: "PASTE_YOUR_PROJECT.appspot.com",
-  messagingSenderId: "PASTE_YOUR_SENDER_ID",
-  appId: "PASTE_YOUR_APP_ID"
+  apiKey: "AIzaSyDaUF6IiyaD3-oJ8pbR3KZzS6-njin-VlU",
+  authDomain: "street-survival-90e16.firebaseapp.com",
+  databaseURL: "https://street-survival-90e16-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "street-survival-90e16",
+  storageBucket: "street-survival-90e16.firebasestorage.app",
+  messagingSenderId: "67221657954",
+  appId: "1:67221657954:web:ec11417cddf2c38d6ff43b",
+  measurementId: "G-HTWXX6J5YN"
 };
 
 // true にするとFirebase通信を使います。

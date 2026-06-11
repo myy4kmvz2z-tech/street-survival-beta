3d702370026c485956bda04d1da51f84c0c1409c
myy4kmvz2z-tech <myy4kmvz2z@privaterelay.appleid.com>
2026-06-11 18:23:23 +0900

    Update Firebase configuration with live credentials

--- firebase-config.js
+++ firebase-config.js
@@ -1,16 +1,12 @@
-// STREET SURVIVAL Firebase Config
-// 本番Firebase接続ON
-
 window.STREET_SURVIVAL_FIREBASE_ENABLED = true;
 
 window.STREET_SURVIVAL_FIREBASE_CONFIG = {
-  apiKey: "ここに運営側と同じ値",
-  authDomain: "ここに運営側と同じ値",
-  databaseURL: "ここに運営側と同じ値",
-  projectId: "ここに運営側と同じ値",
-  storageBucket: "ここに運営側と同じ値",
-  messagingSenderId: "ここに運営側と同じ値",
-  appId: "ここに運営側と同じ値"
+  apiKey: "AIzaSyDaUF6IiyaD3-oJ8pbR3KZzS6-njin-VLU",
+  authDomain: "street-survival-90e16.firebaseapp.com",
+  databaseURL: "https://street-survival-90e16-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "street-survival-90e16",
+  storageBucket: "street-survival-90e16.firebasestorage.app",
+  messagingSenderId: "67221657954",
+  appId: "1:67221657954:web:ec11417cddf2c38d6ff43b",
+  measurementId: "G-HTWXX6J5YN"
 };
-
-console.log("STREET SURVIVAL: Firebase ON / LIVE MODE");

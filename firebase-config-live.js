f3c0124ebeb8293d0f282df526db5383ab4b4f0a
myy4kmvz2z-tech <myy4kmvz2z@privaterelay.appleid.com>
2026-06-11 18:29:05 +0900

    Add Firebase config for STREET SURVIVAL live mode
    Added Firebase configuration for live mode with necessary parameters.

--- firebase-config-live.js
+++ firebase-config-live.js
@@ -1,1 +1,17 @@
-Unsupported Media Type
+// STREET SURVIVAL Firebase Config
+// 本番Firebase接続ON
+
+window.STREET_SURVIVAL_FIREBASE_ENABLED = true;
+
+window.STREET_SURVIVAL_FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDaUF6IiyaD3-oJ8pbR3KZzS6-njin-VLU",
+  authDomain: "street-survival-90e16.firebaseapp.com",
+  databaseURL: "https://street-survival-90e16-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "street-survival-90e16",
+  storageBucket: "street-survival-90e16.firebasestorage.app",
+  messagingSenderId: "67221657954",
+  appId: "1:67221657954:web:ec11417cddf2c38d6ff43b",
+  measurementId: "G-HTWXX6J5YN"
+};
+
+console.log("STREET SURVIVAL: Firebase ON / LIVE MODE");Unsupported Media Type
